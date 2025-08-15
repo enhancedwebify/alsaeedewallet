2b5ff0182c17f55e676d4430a511e7c3a8fe6f51
YUNUS KHAN <enhancedwebify@gmail.com>
2025-08-15 18:44:58 +0300

    Updates, some fixes.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>محفظة العائلة الإلكترونية - الصفحة الرئيسية</title>
+    <title>محفظة آل سعيد الإلكترونية - الصفحة الرئيسية</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
