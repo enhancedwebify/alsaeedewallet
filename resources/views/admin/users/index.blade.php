5e0b03af19f61340e9b4119c0e5e946bf2cb497a
YUNUS KHAN <enhancedwebify@gmail.com>
2025-09-08 15:54:37 +0300

    Updates, some fixes.

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-3">
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h2>إدارة الأعضاء</h2>
-        <a href="{{ route('admin.users.create') }}" class="btn btn-success">إضافة عضو جديد</a>
+        <a href="{{ route('admin.users.create') }}" class="btn btn-primary">إضافة عضو جديد</a>
     </div>
     <hr>
 
